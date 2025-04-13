9d7a5236d2f8e1e8510f98fc77b87cbfb170742f
Amit Yadav <amit22hulk@gmail.com>
2025-04-13 21:56:38 +0530

    Update auth.py

--- auth.py
+++ auth.py
@@ -2,6 +2,8 @@ from fastapi import APIRouter, HTTPException
 from pydantic import BaseModel
 from sqlalchemy import create_engine, Column, Integer, String
 from sqlalchemy.orm import declarative_base, sessionmaker
+import os
+
 
 router = APIRouter()
 
